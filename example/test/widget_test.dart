0bfd442814af60cd0a4f0c0684d5d545897096fc
Santiago Quihui <santiagoquihui@hotmail.com>
2024-08-31 18:40:10 -0700

    rename main file

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:wallet_verifier_api_example/main.dart';
+import 'package:wallet_verifier_api_example/wallet_verifier_api_example.dart';
 
 void main() {
   testWidgets('Verify Platform version', (WidgetTester tester) async {
